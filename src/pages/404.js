c9886a0e8a3c39e01fbb78b82a26bdd8604fc20d
Khaled Garbaya <khaled@contentful.com>
2017-12-19 13:42:21 +0100

    feat: Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>404 Not Found</h1>
+        <p>Oops something went wrong</p>
+      </div>
+    )
+  }
+}
+
+export default NotFound
